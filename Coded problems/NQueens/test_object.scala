11278c74ede7000d85934df6b06e07e9a2d5ead6
akash-07 <akashdhasade@gmail.com>
2017-07-31 15:51:56 +0530

    Added sample solution to nQueens

--- Coded problems/NQueens/test_object.scala
+++ Coded problems/NQueens/test_object.scala
@@ -1,9 +1,25 @@
 package NQueens
 import NQueens._
 
+/* Solution for 4 Queens printed as :
+
+* Q * *
+* * * Q
+Q * * *
+* * Q *
+
+* * Q *
+Q * * *
+* * * Q
+* Q * *
+
+*/
+
+
 object test extends App{
 	val four_queens = new nQueens(4)
 	val solution = four_queens.solution
 	val solution_string = four_queens.getStringForAll(solution).mkString("\n\n")
 	println(solution_string) 
 }
+
